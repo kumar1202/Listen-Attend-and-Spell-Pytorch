b2b9cee2510fc70846bcdcf902c28a36b8b2f3e3
XenderLiu <alexliu36@gmail.com>
2018-06-07 16:57:12 +0800

    Preprocess added normalization

--- util/librispeech_preprocess.py
+++ util/librispeech_preprocess.py
@@ -4,8 +4,8 @@ import os
 import numpy as np
 from tqdm import tqdm
 from joblib import Parallel, delayed
-from python_speech_features import logfbank
 import scipy.io.wavfile as wav
+from python_speech_features import logfbank
 
 import argparse
 
@@ -15,8 +15,14 @@ parser = argparse.ArgumentParser(description='Librispeech preprocess.')
 parser.add_argument('root', metavar='root', type=str,
                      help='Absolute file path to LibriSpeech. (e.g. /usr/downloads/LibriSpeech/)')
 
-parser.add_argument('sets', metavar='sets', type=str, nargs='+',
-                     help='Datasets to process in LibriSpeech. (e.g. train-clean-100/)')
+parser.add_argument('tr_sets', metavar='tr_sets', type=str, nargs='+',
+                     help='Training datasets to process in LibriSpeech. (e.g. train-clean-100/)')
+
+parser.add_argument('--dev_sets', metavar='dev_sets', type=str, nargs='+', default=[] ,
+                     help='Validation datasets to process in LibriSpeech. (e.g. dev-clean/)')
+
+parser.add_argument('--tt_sets', metavar='tt_sets', type=str, nargs='+', default=[] ,
+                     help='Testing datasets to process in LibriSpeech. (e.g. test-clean/)')
 
 parser.add_argument('--n_jobs', dest='n_jobs', action='store', default=-2 ,
                    help='number of cpu availible for preprocessing.\n -1: use all cpu, -2: use all cpu but one')
@@ -24,169 +30,248 @@ parser.add_argument('--n_filters', dest='n_filters', action='store', default=40 
                    help='number of filters for fbank. (Default : 40)')
 parser.add_argument('--win_size', dest='win_size', action='store', default=0.025 ,
                    help='window size during feature extraction (Default : 0.025 [25ms])')
-parser.add_argument('--char_map', dest='char_map', action='store', default=None ,
-                   help='Character2Index mapping file, generated during training data preprocessing. Specify this argument when processing dev/test data.')
 
 
 
 paras = parser.parse_args()
 
 root = paras.root
-libri_path = paras.sets
+train_path = paras.tr_sets
+dev_path = paras.dev_sets
+test_path = paras.tt_sets
 n_jobs = paras.n_jobs
 n_filters = paras.n_filters
 win_size = paras.win_size
-char_map_path = paras.char_map
 
 
-# # flac 2 wav
+def traverse(root,path,search_fix='.flac',return_label=False):
+    f_list = []
 
+    for p in path:
+        p = root + p
+        for sub_p in sorted(os.listdir(p)):
+            for sub2_p in sorted(os.listdir(p+sub_p+'/')):
+                if return_label:
+                    # Read trans txt
+                    with open(p+sub_p+'/'+sub2_p+'/'+sub_p+'-'+sub2_p+'.trans.txt','r') as txt_file:
+                        for line in txt_file:
+                            f_list.append(' '.join(line[:-1].split(' ')[1:]))
+                else:
+                    # Read acoustic feature
+                    for file in sorted(os.listdir(p+sub_p+'/'+sub2_p)):
+                        if search_fix in file:
+                            file_path = p+sub_p+'/'+sub2_p+'/'+file
+                            f_list.append(file_path)   
+    return f_list
 
 def flac2wav(f_path):
     flac_audio = AudioSegment.from_file(f_path, "flac")
     flac_audio.export(f_path[:-5]+'.wav', format="wav")
-
-
-    
-print('Processing flac2wav...',flush=True)
-print(flush=True)
-
-
-file_list = []
-
-for p in libri_path:
-    p = root + p
-    for sub_p in sorted(os.listdir(p)):
-        for sub2_p in sorted(os.listdir(p+sub_p+'/')):
-            for file in sorted(os.listdir(p+sub_p+'/'+sub2_p)):
-                if '.flac' in file:
-                    file_path = p+sub_p+'/'+sub2_p+'/'+file
-                    file_list.append(file_path)
-                    
-
-results = Parallel(n_jobs=n_jobs,backend="threading")(delayed(flac2wav)(i) for i in tqdm(file_list))
-
-                    
-print('done')
-
-
-# # wav 2 log-mel fbank
-
 
 def wav2logfbank(f_path):
     (rate,sig) = wav.read(f_path)
     fbank_feat = logfbank(sig,rate,winlen=win_size,nfilt=n_filters)
     np.save(f_path[:-3]+'fb'+str(n_filters),fbank_feat)
 
-print('Processing wav2logfbank...',flush=True)
-print(flush=True)
+def norm(f_path,mean,std):
+    np.save(f_path,(np.load(f_path)-mean)/std)
 
-results = Parallel(n_jobs=n_jobs,backend="threading")(delayed(wav2logfbank)(i[:-4]+'wav') for i in tqdm(file_list))
+
+print('----------Processing Datasets----------')
+print('Training sets :',train_path)
+print('Validation sets :',dev_path)
+print('Testing sets :',test_path)
+
+
+
+# # flac2wav
+print('---------------------------------------')
+print('Processing flac2wav...',flush=True)
+
+print('Training',flush=True)
+tr_file_list = traverse(root,train_path)          
+results = Parallel(n_jobs=n_jobs,backend="threading")(delayed(flac2wav)(i) for i in tqdm(tr_file_list))
+
+print('Validation',flush=True)
+dev_file_list = traverse(root,dev_path)   
+results = Parallel(n_jobs=n_jobs,backend="threading")(delayed(flac2wav)(i) for i in tqdm(dev_file_list))
+
+print('Testing',flush=True)
+tt_file_list = traverse(root,test_path)   
+results = Parallel(n_jobs=n_jobs,backend="threading")(delayed(flac2wav)(i) for i in tqdm(tt_file_list))
+                  
+
+
+# # wav 2 log-mel fbank
+print('---------------------------------------')
+print('Processing wav2logfbank...',flush=True)
+
+print('Training',flush=True)
+results = Parallel(n_jobs=n_jobs,backend="threading")(delayed(wav2logfbank)(i[:-4]+'wav') for i in tqdm(tr_file_list))
+
+print('Validation',flush=True)
+results = Parallel(n_jobs=n_jobs,backend="threading")(delayed(wav2logfbank)(i[:-4]+'wav') for i in tqdm(dev_file_list))
+
+print('Testing',flush=True)
+results = Parallel(n_jobs=n_jobs,backend="threading")(delayed(wav2logfbank)(i[:-4]+'wav') for i in tqdm(tt_file_list))
                     
-print('done')   
 
 
 # # log-mel fbank 2 feature
+print('---------------------------------------') 
+print('Preparing Training Dataset...',flush=True)
 
-print('Preparing dataset...',flush=True)
-
-file_list = []
-text_list = []
-
-for p in libri_path:
-    p = root + p
-    for sub_p in sorted(os.listdir(p)):
-        for sub2_p in sorted(os.listdir(p+sub_p+'/')):
-            # Read trans txt
-            with open(p+sub_p+'/'+sub2_p+'/'+sub_p+'-'+sub2_p+'.trans.txt','r') as txt_file:
-                for line in txt_file:
-                    text_list.append(' '.join(line[:-1].split(' ')[1:]))
-            # Read acoustic feature
-            for file in sorted(os.listdir(p+sub_p+'/'+sub2_p)):
-                if '.fb'+str(n_filters) in file:
-                    file_path = p+sub_p+'/'+sub2_p+'/'+file
-                    file_list.append(file_path)
-
+tr_file_list = traverse(root,train_path,search_fix='.fb'+str(n_filters))
+tr_text = traverse(root,train_path,return_label=True)
 
 X = []
-for f in file_list:
+for f in tr_file_list:
     X.append(np.load(f))
 
+# Normalize X
+mean_x = np.mean(np.concatenate(X,axis=0),axis=0)
+std_x = np.std(np.concatenate(X,axis=0),axis=0)
 
-
-audio_len = [len(x) for x in X]
-
+results = Parallel(n_jobs=n_jobs,backend="threading")(delayed(norm)(i,mean_x,std_x) for i in tqdm(tr_file_list))
 
 
 # Sort data by signal length (long to short)
-file_list = [file_list[idx] for idx in reversed(np.argsort(audio_len))]
-text_list = [text_list[idx] for idx in reversed(np.argsort(audio_len))]
+audio_len = [len(x) for x in X]
 
+tr_file_list = [tr_file_list[idx] for idx in reversed(np.argsort(audio_len))]
+tr_text = [tr_text[idx] for idx in reversed(np.argsort(audio_len))]
 
-if char_map_path:
-    # Load char mapping
-    char_map = {}
-    with open(char_map_path,'r') as f:
-        for line in f:
-            if 'idx,char' in line:
-                continue
-            idx = int(line.split(',')[0])
-            char = line[:-1].split(',')[1]
-            char_map[char] = idx
-    
-else:
-    assert 'train' in libri_path[0]
-    # Create char mapping
-    char_map = {}
-    char_map['<sos>'] = 0
-    char_map['<eos>'] = 1
-    char_idx = 2
+# Create char mapping
+char_map = {}
+char_map['<sos>'] = 0
+char_map['<eos>'] = 1
+char_idx = 2
 
-    # map char to index
-    for text in text_list:
-        for char in text:
-            if char not in char_map:
-                char_map[char] = char_idx
-                char_idx +=1
-                
-    # Reverse mapping
-    rev_char_map = {v:k for k,v in char_map.items()}
+# map char to index
+for text in tr_text:
+    for char in text:
+        if char not in char_map:
+            char_map[char] = char_idx
+            char_idx +=1
+            
+# Reverse mapping
+rev_char_map = {v:k for k,v in char_map.items()}
 
-    # Save mapping
-    with open(root+'idx2chap.csv','w') as f:
-        f.write('idx,char\n')
-        for i in range(len(rev_char_map)):
-            f.write(str(i)+','+rev_char_map[i]+'\n')
+# Save mapping
+with open(root+'idx2chap.csv','w') as f:
+    f.write('idx,char\n')
+    for i in range(len(rev_char_map)):
+        f.write(str(i)+','+rev_char_map[i]+'\n')
 
 # text to index sequence
 tmp_list = []
-for text in text_list:
+for text in tr_text:
     tmp = []
     for char in text:
         tmp.append(char_map[char])
     tmp_list.append(tmp)
-text_list = tmp_list
+tr_text = tmp_list
 del tmp_list
 
-
 # write dataset
-
-
-if 'train' in libri_path[0]:
-    file_name = 'train.csv'
-elif 'test' in libri_path[0]:
-    file_name = 'test.csv'
-elif 'dev' in libri_path[0]:
-    file_name = 'dev.csv'
+file_name = 'train.csv'
 
 print('Writing dataset to '+root+file_name+'...',flush=True)
 
 with open(root+file_name,'w') as f:
     f.write('idx,input,label\n')
-    for i in range(len(file_list)):
+    for i in range(len(tr_file_list)):
         f.write(str(i)+',')
-        f.write(file_list[i]+',')
-        for char in text_list[i]:
+        f.write(tr_file_list[i]+',')
+        for char in tr_text[i]:
             f.write(' '+str(char))
         f.write('\n')
-print('done')
+
+print()
+print('Preparing Validation Dataset...',flush=True)
+
+dev_file_list = traverse(root,dev_path,search_fix='.fb'+str(n_filters))
+dev_text = traverse(root,dev_path,return_label=True)
+
+X = []
+for f in dev_file_list:
+    X.append(np.load(f))
+
+# Normalize X
+results = Parallel(n_jobs=n_jobs,backend="threading")(delayed(norm)(i,mean_x,std_x) for i in tqdm(dev_file_list))
+
+
+# Sort data by signal length (long to short)
+audio_len = [len(x) for x in X]
+
+dev_file_list = [dev_file_list[idx] for idx in reversed(np.argsort(audio_len))]
+dev_text = [dev_text[idx] for idx in reversed(np.argsort(audio_len))]
+
+# text to index sequence
+tmp_list = []
+for text in dev_text:
+    tmp = []
+    for char in text:
+        tmp.append(char_map[char])
+    tmp_list.append(tmp)
+dev_text = tmp_list
+del tmp_list
+
+# write dataset
+file_name = 'dev.csv'
+
+print('Writing dataset to '+root+file_name+'...',flush=True)
+
+with open(root+file_name,'w') as f:
+    f.write('idx,input,label\n')
+    for i in range(len(dev_file_list)):
+        f.write(str(i)+',')
+        f.write(dev_file_list[i]+',')
+        for char in dev_text[i]:
+            f.write(' '+str(char))
+        f.write('\n')
+
+print()
+print('Preparing Testing Dataset...',flush=True)
+
+test_file_list = traverse(root,test_path,search_fix='.fb'+str(n_filters))
+tt_text = traverse(root,test_path,return_label=True)
+
+X = []
+for f in test_file_list:
+    X.append(np.load(f))
+
+# Normalize X
+results = Parallel(n_jobs=n_jobs,backend="threading")(delayed(norm)(i,mean_x,std_x) for i in tqdm(test_file_list))
+
+
+# Sort data by signal length (long to short)
+audio_len = [len(x) for x in X]
+
+test_file_list = [test_file_list[idx] for idx in reversed(np.argsort(audio_len))]
+tt_text = [tt_text[idx] for idx in reversed(np.argsort(audio_len))]
+
+# text to index sequence
+tmp_list = []
+for text in tt_text:
+    tmp = []
+    for char in text:
+        tmp.append(char_map[char])
+    tmp_list.append(tmp)
+tt_text = tmp_list
+del tmp_list
+
+# write dataset
+file_name = 'test.csv'
+
+print('Writing dataset to '+root+file_name+'...',flush=True)
+
+with open(root+file_name,'w') as f:
+    f.write('idx,input,label\n')
+    for i in range(len(test_file_list)):
+        f.write(str(i)+',')
+        f.write(test_file_list[i]+',')
+        for char in tt_text[i]:
+            f.write(' '+str(char))
+        f.write('\n')
+
--- util/librispeech_preprocess.sh
+++ util/librispeech_preprocess.sh
@@ -2,6 +2,4 @@ if [ "$#" -ne 1 ]; then
     echo "Usage : ./librispeech_preprocess.sh <LibriSpeech folder>"
 fi
 
-python3 librispeech_preprocess.py $1 train-clean-100/
-python3 librispeech_preprocess.py --char_map $1/idx2chap.csv $1 dev-clean/
-python3 librispeech_preprocess.py --char_map $1/idx2chap.csv $1 test-clean/
+python3 librispeech_preprocess.py $1 train-clean-100/ --dev_sets dev-clean/ --tt_sets test-clean/
